7cb9b39ec9df0e4ebe0c681ff876ffc595be705d
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-10-04 09:18:34 +0330

    fix folders

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Navbar from "@/components/modules/navbar/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ src/components/modules/navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <h1>Navbar Module</h1>;
+}
+
+export default Navbar;
