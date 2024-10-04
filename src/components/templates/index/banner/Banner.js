6cd359450e57876f612ad8fc650fda7d2b3d161f
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-10-04 10:26:11 +0330

    finish promote sections and start footer sections

---
+++ src/components/templates/index/banner/Banner.js
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation, Autoplay } from "swiper/modules";
+
+function Banner() {
+  return (
+    <Swiper
+      rewind={true}
+      navigation={true}
+      loop={true}
+      autoplay={{ delay: 1500 }}
+      modules={[Navigation, Autoplay]}
+      className="mySwiper home-slider"
+    >
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2023/12/slide.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2021/10/winter-slie.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://set-coffee.com/wp-content/uploads/2022/06/fall.jpg"
+          alt="Slide"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
+
+export default Banner;
---
+++ src/components/templates/index/latest/Lates.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Lates() {
+  return (
+    <div>Lates</div>
+  )
+}
+
+export default Lates
